241fe2f8fc0ed71317fe4b98e86f635f552e8da5
Donald Shen <donaldmyshen@outlook.com>
2019-10-10 00:38:08 -0500

    redeploy and update some logic

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mingyuan/Documents/GitHub/Books-Renting-Site-with-Django/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
